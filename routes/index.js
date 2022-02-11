3db19ca4fc8324cf0096492680b3f8a9342eb072
unknown <abhi10012001@gmail.com>
2022-02-12 00:38:41 +0530

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get("/", function (req, res, next) {
     res.render("index", { title: "Express" });
 });
 
-router.get("/driver", function (req, res, next) {
+router.get("/home", function (req, res, next) {
     res.render("driver", { title: "Driver" });
 });
 
