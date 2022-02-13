06c4227a63c91cdaff3e676fb8713eaa7ee9094c
Sujiv Raj <59267376+sujiv1204@users.noreply.github.com>
2022-02-13 14:50:50 +0530

    dealer css update

--- routes/dealerRoute.js
+++ routes/dealerRoute.js
@@ -2,22 +2,21 @@ var express = require("express");
 const driverModel = require("../models/driver");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('driver', {title: 'Dealer'});
-})
-
-router.get('/register', function(req, res, next) {
-    res.render('dealer_register');
+router.get("/", function (req, res, next) {
+    res.render("dealer", { title: "Dealer" });
 });
 
-router.post('/register', function(req, res) {
-  var email = req.body.email;
-  var password = req.body.password;
-  
+router.get("/register", function (req, res, next) {
+    res.render("dealer_register");
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.post("/register", function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+});
+
+router.get("/login", function (req, res, next) {
+    res.render("login");
 });
 
 module.exports = router;
