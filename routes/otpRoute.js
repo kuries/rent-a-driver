c9f3aac357b5cf3d3fcc08bcba9d7dfb14b229c8
pranav <pondurupranav2020@gmail.com>
2022-02-13 17:28:19 +0530

    login route problem,

--- routes/otpRoute.js
+++ routes/otpRoute.js
@@ -59,7 +59,7 @@ router.post('/login', async (req, res) => {
 
     if(!ret_dealer && !ret_driver)
     {
-        res.render('/', {err_msg: "Email not registered"});
+        res.redirect('/');
     }
 
     const myPlaintextPassword = generator.generate({
