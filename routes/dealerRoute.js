9cb6f7f7f6aa20578a13ebf7d2492434851b3b8b
Sujiv Raj <59267376+sujiv1204@users.noreply.github.com>
2022-02-13 23:53:11 +0530

    add dealer booked page

--- routes/dealerRoute.js
+++ routes/dealerRoute.js
@@ -4,38 +4,29 @@ const dealerModel = require("../models/dealer");
 const driverModel = require("../models/driver");
 var router = express.Router();
 
-
-function authenticateDealer(req, res, next)
-{
-	if(req.session.email && req.session.designation == "dealer")
-	{
-		next();
-	}
-	else
-	{
-		return res.redirect('/');
-	}
+function authenticateDealer(req, res, next) {
+    if (req.session.email && req.session.designation == "dealer") {
+        next();
+    } else {
+        return res.redirect("/");
+    }
 }
 
-function unauthenticateDealer(req, res, next)
-{
-	if(req.session.email && req.session.designation == "dealer")
-	{
-		return res.redirect('/');
-	}
-	else
-	{
-		next();
-	}
+function unauthenticateDealer(req, res, next) {
+    if (req.session.email && req.session.designation == "dealer") {
+        return res.redirect("/");
+    } else {
+        next();
+    }
 }
 
-router.get('/register', unauthenticateDealer, function(req, res, next) {
-    res.render('dealer_register', {check: false});
+router.get("/register", unauthenticateDealer, function (req, res, next) {
+    res.render("dealer_register", { check: false });
 });
 
 router.post("/register", async function (req, res) {
     hashedPassword = await bcrypt.hash(req.body.password, 10);
-	req.body.password = hashedPassword;
+    req.body.password = hashedPassword;
     const new_dealer = new dealerModel(req.body);
 
     new_dealer.save(function (err, result) {
@@ -62,35 +53,47 @@ router.get("/", authenticateDealer, async function (req, res) {
         title: "Dealer",
         name: val.name,
         result: data,
-        check: true
+        check: true,
     });
 });
 
-router.get('/login', unauthenticateDealer, function(req, res, next) {
-  res.render('login', {
-      id: "dealer",
-      check: false
-  });
+router.get("/booked", authenticateDealer, async function (req, res) {
+    const val = await dealerModel.findOne({ email: "asdf@gmail.com" }).exec();
+    const data = await driverModel.find({}).exec();
+    // console.log(data);
+
+    res.render("booked", {
+        title: "Dealer",
+        name: val.name,
+        result: data,
+        check: true,
+    });
 });
 
-router.post('/login', async function(req, res) {
+router.get("/login", unauthenticateDealer, function (req, res, next) {
+    res.render("login", {
+        id: "dealer",
+        check: false,
+    });
+});
+
+router.post("/login", async function (req, res) {
     console.log(req.body.email);
-	const user = dealerModel.findOne({ email: req.body.email }).exec();
-	var isValid = await user.then(docs => {
+    const user = dealerModel.findOne({ email: req.body.email }).exec();
+    var isValid = await user.then((docs) => {
         return bcrypt
             .compare(req.body.password, docs.password)
-            .then(result => {
+            .then((result) => {
                 return result;
             })
-            .catch (err => console.log(err) )
+            .catch((err) => console.log(err));
     });
-	if (isValid)
-	{
-		req.session.email = req.body.email;
-		req.session.designation = 'dealer';
-		req.session.save();
-		res.redirect('/dealer');
-	}
+    if (isValid) {
+        req.session.email = req.body.email;
+        req.session.designation = "dealer";
+        req.session.save();
+        res.redirect("/dealer");
+    }
 });
 
 router.get("/data", async (request, response) => {
